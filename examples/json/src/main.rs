107acfec6b85e234b1da2b48637e8da78a8b5dc2
yportne <yportne@163.com>
2022-10-16 22:35:55 +0800

    feat(json):give a json example

--- examples/json/src/main.rs
+++ examples/json/src/main.rs
@@ -11,20 +11,37 @@ enum JSON {
     JObject(HashMap<String, JSON>)
 }
 
-fn json_parser(input: &str) -> (Option<f64>, &str) {
+fn json_parser(input: &str) -> (Option<JSON>, &str) {
     let whitespace = ParseRegex(r"\s*");
     let head = whitespace >> Token("{") << whitespace;
     let tail = whitespace >> Token("}") << whitespace;
     let double = ParseRegex(r"[-+]?([0-9]*\.)?[0-9]+([eE][-+]?[0-9]+)?").map(|x| x.parse::<f64>().unwrap());
     let escaped_quoted = Token("\"") >> ParseRegex(r#"([^"]*)"#) << Token("\"");
     
-    let ret = head >> double << tail;
-    //ret.map(|x| JSON::JNumber(x)).run(input)
-    ret.run_with_out(input)
+    let lit_temp =
+        (Token("null") << whitespace).map(|_| JSON::JNull) |
+        (double << whitespace).map(JSON::JNumber) |
+        (escaped_quoted << whitespace).map(JSON::JString) |
+        (Token("true") << whitespace).map(|_| JSON::JBool(true)) |
+        (Token("false") << whitespace).map(|_| JSON::JBool(false));
+    let lit = whitespace >> lit_temp << whitespace;
+    let array = 
+        (whitespace >> Token("[") << whitespace) >>
+        Many(lit, Some(",")).map(JSON::JArray)//TODO: lit => value
+        << (whitespace >> Token("]") << whitespace);
+    let value_temp =
+        whitespace >> ((escaped_quoted << whitespace << Token(":")) *
+        (lit | array)) << whitespace;//TODO: (lit | array) => (lit | array | obj)
+    let value = whitespace >> value_temp << whitespace;
+    let obj =
+        head >>
+        Many(value, Some(",")).map(|x| JSON::JObject(x.into_iter().collect::<HashMap<String, JSON>>()))
+        << tail;
+
+    obj.run_with_out(input)
 }
 
 fn main() {
-    println!("Hello, world!");
     let input = r#"
 {
   "Company name" : "Microsoft Corporation",
@@ -35,11 +52,7 @@ fn main() {
   "Related companies" : [ "HPQ", "IBM", "YHOO", "DELL", "GOOG" ]
 }
 "#;
-    let test = r#"
-    {
-        30.66
-    }
-    "#;
-    let ret = json_parser(test);
-    println!("{:?}", ret)
+    let ret = json_parser(input);
+    println!("{:?}", ret.0);
+    println!("{:?}", ret.1);
 }
